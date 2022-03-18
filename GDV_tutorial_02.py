e25be1f61a9fe1f9c5855de4b389d5d2bc265138
uhahne <uwe.hahne@hs-furtwangen.de>
2022-03-18 16:35:31 +0100

    Improvements on tut02

--- GDV_tutorial_02.py
+++ GDV_tutorial_02.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # loading images in grey and color
 img_gray = cv2.imread('images/logo.png', cv2.IMREAD_GRAYSCALE)
@@ -8,11 +9,11 @@ img_color = cv2.imread('images/logo.png', cv2.IMREAD_COLOR)
 print(type(img_gray))  # prints class numpy.ndarray
 print(type(img_color))  # prints class numpy.ndarray
 
+print(img_gray.dtype)  # prints the data type of the image array
+print(img_color.dtype)  # prints the date type of the image array
+
 print(img_gray.shape)  # prints the size of the image array
 print(img_color.shape)  # prints the size of the image array
-
-# Continue with the color image
-# img = imgColor
 
 # Continue with the grayscale image
 # img = img_gray
@@ -21,36 +22,45 @@ img = img_gray.copy()
 # Extract the size or resolution of the image
 height = img.shape[0]
 width = img.shape[1]
+last_dim = img.shape[-1]
 print('height = ' + str(img.shape[0]))
 print('width = ' + str(img.shape[1]))
+print('last dimension = ' + str(last_dim))
 
 # resize image
-# new_width = 7
-# new_height = 5
-# new_size = (new_width, new_height)
-# img = cv2.resize(img, new_size)
+new_width = 7
+new_height = 5
+new_size = (new_width, new_height)
+img = cv2.resize(img, new_size)
 
 # row and column access, see
 #   https://numpy.org/doc/stable/reference/arrays.ndarray.html
 #   for general access on ndarrays
 # print first row
-# print (img[0])
+print(img[0])
 # print first column
-# print (img[:,0])
+print(img[:, 0])
+
+# continue with the color image
+img = img_color.copy()
 
 # set area of the image to black
-# for i in range (30):
-#     for j in range (width):
-#         img[i][j] = [0,0,0]
+for i in range(30):
+    for j in range(width):
+        img[i][j] = [0, 0, 0]
+
+# show the image
+title = 'OpenCV Python Tutorial'
+# Note that window parameters have no effect on MacOS
+cv2.namedWindow(title, cv2.WINDOW_AUTOSIZE)
+cv2.imshow(title, img)
+cv2.waitKey(0)
 
 # find all used colors in the image
-colors = []
-for i in range(height):
-    for j in range(width):
-        curr_color = img[i, j]
-        if colors.count(curr_color) == 0:
-            colors.append(curr_color)
-print('Those gray values are in the image:\n ' + str(colors))
+# source: https://stackoverflow.com/questions/51705187/list-of-all-colors-in-an-image-using-opencv-and-python
+all_rgb_codes = img.reshape(-1, img.shape[-1])
+unique_rgb_codes = np.unique(all_rgb_codes, axis=0, return_counts=False)
+print('Those color values are in the image:\n ' + str(unique_rgb_codes))
 
 # copy one part of an image into another one
 letters = img[30:105, 5:130]
@@ -59,10 +69,7 @@ img[115:190, 150:275] = letters
 # save image
 cv2.imwrite('img_tutorial02.jpg', img)
 
-# show the image
-title = 'OpenCV Python Tutorial'
-# Note that window parameters have no effect on MacOS
-cv2.namedWindow(title, cv2.WINDOW_AUTOSIZE)
+# show the image again
 cv2.imshow(title, img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
@@ -70,6 +77,6 @@ cv2.destroyAllWindows()
 # show the original image (copy demo)
 title = 'How a copy works'
 cv2.namedWindow(title, cv2.WINDOW_AUTOSIZE)
-cv2.imshow(title, img_gray)
+cv2.imshow(title, img_color)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- GDV_tutorial_02_empty.py
+++ GDV_tutorial_02_empty.py
@@ -1,29 +1,36 @@
 import cv2
+import numpy as np
 
-# TODO load images in grey and color
+# TODO loading images in grey and color
 
-# TODO do some print out about the loaded data
+# TODO do some print out about the loaded data using type, dtype and shape
 
-
-# TODO Continue with the color image or the grayscale image
-
+# TODO Continue with the grayscale image
 
 # TODO Extract the size or resolution of the image
 
 # TODO resize image
 
+# row and column access, see
+#   https://numpy.org/doc/stable/reference/arrays.ndarray.html
+#   for general access on ndarrays
 # TODO print first row
 
 # TODO print first column
 
+
+# TODO continue with the color image
+
 # TODO set an area of the image to black
+
+# TODO show the image and wait until key pressed
 
 # TODO find all used colors in the image
 
 # TODO copy one part of an image into another one
 
-# TODO save image
+# TODO save image to a file
 
-# TODO show the image
+# TODO show the image again
 
 # TODO show the original image (copy demo)
